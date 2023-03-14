b108cf60058f5e31d7a1003a85f60df4c00ec4b5
Diana Itzel De la o Veracruz <l19350640@tuxtepec.tecnm.mx>
2023-03-14 13:38:52 -0600

    modificacion 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 
+const buttonsClasses = 'btn btn-primary w-75 mt-2';
+
 function App() {
+  const [screen, setScreen] = useState('0');
+
+  const handleButtonClick = () => {
+  if (screen === '0') {
+    setScreen('9');
+  } else {
+    setScreen(`${screen}9`);
+  }
+};
+
   return (
     <div className='app'>
     <h1>Calculator</h1>
@@ -15,55 +28,69 @@ function App() {
         border: '1px solid black',
         textAlign: 'end',
       }}>
-        <h2>0</h2>
+        <h2>{screen}</h2>
       </td>
       </tr>
 
       <tr>
        <td><button type='button'
-        className='btn btn-primary w-75 mt-2'>C</button></td>
+        className={buttonsClasses}
+        >C</button></td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>/</button></td>
+       className={buttonsClasses}
+       >/</button></td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>*</button></td>
+       className={buttonsClasses}
+       >*</button></td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>-</button></td>
+       className={buttonsClasses}
+       >-</button></td>
        </tr>
 
       <tr>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>7</button></td>
+       className={buttonsClasses}
+       >7</button></td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>8</button></td>
+       className={buttonsClasses}
+       >8</button></td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>9</button></td>
+       className={buttonsClasses}
+       onClick={handleButtonClick}
+       >9</button></td>
        <td rowSpan={2}><button type='button'
-       className='btn btn-primary w-75 mt-2'
+       className={buttonsClasses}
        style={{height: "85px"}}
        >+</button></td>
        </tr>
 
       <tr>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>4</button></td>
+       className={buttonsClasses}
+       >4</button></td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>5</button></td>
+       className={buttonsClasses}
+       >5</button></td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>6</button></td>
+       className={buttonsClasses}
+       >6</button></td>
        </tr>
 
       <tr>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>1</button>
+       className={buttonsClasses}
+       >1</button>
        </td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>2</button>
+       className={buttonsClasses}
+       >2</button>
        </td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>3</button>
+       className={buttonsClasses}
+       >3</button>
        </td>
        <td rowSpan={2}><button type='button'
-       className='btn btn-primary w-75 mt-2'
+       className={buttonsClasses}
        style={{height: "85px"}}
        >=</button>
        </td>
@@ -73,14 +100,16 @@ function App() {
       <td>
         <button
         type='button'
-        className='btn btn-primary mt-2 w-75'>DEL</button>
+        className={buttonsClasses}
+        >DEL</button>
       </td>
        <td>
         <button type='button'
-       className='btn btn-primary mt-2 w-75' 
+       className={buttonsClasses} 
        >0</button></td>
        <td><button type='button'
-       className='btn btn-primary w-75 mt-2'>.</button></td>
+       className={buttonsClasses}
+       >.</button></td>
        </tr>
     </table>
     </div>
